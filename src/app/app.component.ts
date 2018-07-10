21b5c5d1f64ae1e464209a210da830579e7e4486
Naresh Chakilam <Naresh.Chakilam@T-Mobile.com>
2018-07-10 13:30:08 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { AppService } from './app.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  public customerData;
+  public objectKeys;
+  constructor(private _appService: AppService) {
+    this._appService.getCustomerData().subscribe(data => {
+      data.sheet1.splice(0, 1 )
+      this.customerData = data.sheet1;
+      console.log(this.customerData);
+    });
+  }
+  selectedCustomerId: string = null;
+  ngOnInit() {
+    console.log('App Started');
+
+  }
+  private onSelect (value: any ) {
+
+    this.objectKeys = Object.keys(value);
+
+  }
+
 }
--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,4 +1,38 @@
 /**
  * Created by Naresh Chakilam on 7/10/18.
  */
+
+
 'use strict';
+
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {Observable, of} from 'rxjs';
+import {catchError, tap} from 'rxjs/operators';
+
+@Injectable()
+export class AppService {
+  private customerUrl = 'assets/mydata.json';
+   constructor(private http: HttpClient) {
+   }
+   getCustomerData(): Observable<any> {
+     return this.http.get<any>(this.customerUrl)
+       .pipe(
+         catchError(this.handleError('Error', []))
+       );
+   }
+
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      console.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
+  }
+}
